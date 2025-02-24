089a783202426c83c9e6e47c1312250afb0b7104
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-02-25 05:18:35 +0530

    added User component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Hello Nex js App</h1>
+      <User name="Ashwini" />
+
+      <User name="Nishant" />
+      <User name="Ketan" />
+      <User name="Omen" />
     </div>
   );
 }
+
+const User = (props) => {
+  return <h3>Hello {props.name}</h3>;
+};
