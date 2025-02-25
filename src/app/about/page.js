5970294dfa22abc9f2723b1c8a70944b93554068
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-02-25 06:36:01 +0530

    added Link and Navigation in next.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,12 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 const About = () => {
+  const router = useRouter();
   return (
     <>
       <h4>About Page</h4>
+      <button onClick={() => router.push("/")}>Go to Home Page</button>
     </>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
 "use client";
 import { useState } from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const navigateTo = (pathName) => {
+    router.push(pathName);
+  };
   return (
     <div className={styles.page}>
       <h1>Hello Nex js App</h1>
@@ -12,6 +18,12 @@ export default function Home() {
       <User name="Ketan" />
       <User name="Omen" />
       <Counter />
+      <Link href={"/login"}>Go to Login Page</Link>
+      <Link href={"/about"}>Go to About Page</Link>
+
+      <button onClick={() => navigateTo("/login")}>Go to Login Page</button>
+      <button onClick={() => navigateTo("/about")}>Go to About Page</button>
+
       {/* {Counter()}//avoid to use */}
     </div>
   );
