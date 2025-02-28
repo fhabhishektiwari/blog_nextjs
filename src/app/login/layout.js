a6ed44db1fcb3034f858dab46912eb68ac6781d8
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-02-28 06:04:01 +0530

    Conditional Layout in next.js

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,18 +1,26 @@
+"use client";
 import Link from "next/link";
 import "./login.css";
+import { usePathname } from "next/navigation";
 export default function Layout({ children }) {
+  const pathName = usePathname();
+  console.log(pathName);
   return (
     <div>
       <ul className="menu-link">
         <li>
           <Link href={"/login"}>Login</Link>
         </li>
-        <li>
-          <Link href={"/login/studentlogin"}>Student Login</Link>
-        </li>
-        <li>
-          <Link href={"/login/teacherlogin"}>Teacher Login</Link>
-        </li>
+        {pathName !== "/login/teacherlogin" && (
+          <li>
+            <Link href={"/login/studentlogin"}>Student Login</Link>
+          </li>
+        )}
+        {pathName !== "/login/studentlogin" && (
+          <li>
+            <Link href={"/login/teacherlogin"}>Teacher Login</Link>
+          </li>
+        )}
       </ul>
       {children}
     </div>
