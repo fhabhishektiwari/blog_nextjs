ea75fa6bf7540d978e4ddd3e8b091d6c2b72b678
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-03-06 05:33:56 +0530

    Middleware in next.js

--- src/middleware.js
+++ src/middleware.js
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   // console.log("middleware");
   // if (request.nextUrl.pathname != "/login") {
-    return NextResponse.redirect(new URL("/login", request.url));
+  return NextResponse.redirect(new URL("/login", request.url));
   // }
 }
 
 export const config = {
-  matcher: ["/about/:path*", "/studentlist/:path*"],
+  matcher: ["/about/:path+", "/studentlist/:path*"], ///about/:path*
 };
