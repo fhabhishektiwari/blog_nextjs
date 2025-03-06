c6d8b719d42bd11e77dcf535caa1faf8683bf4d4
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-03-07 05:15:06 +0530

    image Optimization in next.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
@@ -25,6 +26,18 @@ export default function Home() {
       <button onClick={() => navigateTo("/about")}>Go to About Page</button>
 
       {/* {Counter()}//avoid to use */}
+
+      {/* Image Optimization */}
+      <Image
+        src={
+          "https://images.unsplash.com/photo-1559981421-3e0c0d712e3b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YW5pbWV8ZW58MHx8MHx8fDA%3D"
+        }
+        width={200}
+        height={200}
+        alt="anime image"
+      />
+
+      <img src="https://images.unsplash.com/photo-1559981421-3e0c0d712e3b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YW5pbWV8ZW58MHx8MHx8fDA%3D" />
     </div>
   );
 }
