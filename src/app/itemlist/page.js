ecc65ef6c9f55ed6a683e547b9ff86c23391d1d1
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-03-06 06:24:48 +0530

    Call API in Server component in next.js

---
+++ src/app/itemlist/page.js
@@ -0,0 +1,20 @@
+async function fetchData() {
+  let data = await fetch("https://dummyjson.com/products");
+  data = await data.json();
+  // console.log(data.products);
+  return data.products;
+}
+export default async function Itemlist() {
+  const products = await fetchData();
+  return (
+    <div>
+      <h2>Item List: Serverside Api call</h2>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h6>Name: {product.title}</h6>
+          <p>Price: {product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
