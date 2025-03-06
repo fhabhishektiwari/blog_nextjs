5aeced5f77f9dc206551d138623280314570870f
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-03-06 12:27:11 +0530

    CSS Modules with Next.js

---
+++ src/app/customuser/page.js
@@ -0,0 +1,11 @@
+import custom from "../custom.module.css";
+import outside from "../../styles/outside.module.css";
+export default function Customuser() {
+  return (
+    <div>
+      <h1 className={custom.main}>Heading 1 custom User Page</h1>
+      <h2 className={outside.main}>Heading 1 custom User Page</h2>
+      <p className="main">This is paragraph</p>
+    </div>
+  );
+}
