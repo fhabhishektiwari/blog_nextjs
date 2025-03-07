03e3acf41c877fa6481750076cd67b8e7c89e454
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-03-07 05:48:56 +0530

    added generateMetadata for dynamic in next js

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,17 +1,28 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 
+// export const metadata = {
+//   title: "user",
+//   description: "user page",
+// };
+
+export function generateMetadata() {
+  return {
+    title: "user",
+    description: "user Page",
+  };
+}
 export default function User() {
-  const [h3Color, setH3Color] = useState({ backgroundColor: "red" });
+  // const [h3Color, setH3Color] = useState({ backgroundColor: "red" });
   return (
     <div>
       <h1 style={{ color: "blue" }}>User Page</h1>
       <h2>Heading 2 in user page</h2>
-      <h3 style={h3Color}>Heading 3 in user page</h3>
-      <button onClick={() => setH3Color({ backgroundColor: "yellow" })}>
+      {/* <h3 style={h3Color}>Heading 3 in user page</h3> */}
+      {/* <button onClick={() => setH3Color({ backgroundColor: "yellow" })}>
         Change Color
-      </button>
+      </button> */}
     </div>
   );
 }
