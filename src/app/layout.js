31b19446ee1ba7d2b78d8a7c3d12b052a3379dc7
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-03-08 11:21:52 +0530

    added Static Assets in next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,12 +21,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="stylesheet" href="/style.css" />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         ></link> */}
+        <script src="/common.js" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
